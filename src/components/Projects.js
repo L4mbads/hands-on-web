00753103885ee4a5ca2c5ec7f15b42842d7221b1
L4mbads <rizacal.mamen@gmail.com>
2024-08-09 21:28:30 +0700

    feat. democrussy

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import shader1 from "../assets/projects/shader1.png";
 import shader2 from "../assets/projects/shader2.png";
 import shader3 from "../assets/projects/shader3.png";
+import game1 from "../assets/projects/game1.png";
+import game2 from "../assets/projects/game2.png";
+import game3 from "../assets/projects/game3.png";
 
 
 function mod(n, m) {
@@ -17,6 +20,12 @@ const shaderImages = [
 
 ];
 
+const gameImages = [
+    game1,
+    game2,
+    game3,
+];
+
 function ImagesComponent(props) {
     const [selectedImage, setImage] = useState(0);
     const nextImage = () => {
@@ -26,7 +35,7 @@ function ImagesComponent(props) {
         <img
             src={props.images[selectedImage]}
             alt="Example"
-            className="aspect-video lg:aspect-square lg:w-1/2 lg:h-auto object-cover mb-4 lg:mb-0"
+            className="aspect-video lg:aspect-square lg:w-1/2 lg:h-screen object-cover"
             onClick={nextImage}
         />
     )
@@ -42,18 +51,17 @@ export default function Projects() {
                         Projects
                     </h1>
                     <p className="mx-auto leading-relaxed text-base">
-                        I take pictures in Minecraft. I started around 2021 without any knowledge on
-                        photography beforehand.
+                        These are the projects that showcase my skills.
+                        Click on the images to view the next one.
                     </p>
                 </div>
-
-
             </div>
+
             <div className="flex flex-col lg:flex-row items-center lg:items-start justify-between bg-white">
                 <ImagesComponent images={shaderImages} />
-                <div className="w-full lg:w-2/3 p-8 ">
+                <div className="w-full lg:w-1/2 p-8 ">
                     <h2 className="text-2xl font-bold text-black">Minecraft Raytraced Shader</h2>
-                    <p className="mt-8 text-lg text-gray-600 xl:max-h-full max-h-96 overflow-y-auto text-justify">
+                    <p className="mt-8 text-lg text-gray-600 text-justify">
                         A real-time raytraced shader with photorealistic lighting including
                         world-space diffuse lighting and reflection,
                         made possible with temporal resampling and a spatio-temporal filter,
@@ -65,13 +73,33 @@ export default function Projects() {
                         Thanks to Iris Shaders mod,
                         I didn't have to mess with the internal Minecraft rendering pipeline and instead,
                         let Iris Shaders inject the GLSL code into Minecraft.
-
-                        Sadly, the language which Minecraft is coded in, Java,
-                        makes it not possible for us to use hardware acceleration RT.
+                        Sadly, the language Minecraft is coded in, Java,
+                        makes it not possible to use hardware acceleration RT.
                         <br />
                         <br />
                         Nevertheless, it was a huge experience for me because i learnt a lot
                         of thing regarding real-time PBR rendering.
+                    </p>
+                </div>
+            </div>
+            <div className="flex flex-col lg:flex-row-reverse items-center lg:items-start justify-between bg-gray-900">
+                <ImagesComponent images={gameImages} />
+                <div className="w-full lg:w-1/2 p-8 ">
+                    <h2 className="text-2xl font-bold text-white">Democrussy</h2>
+                    <p className="mt-8 text-lg text-gray-400 text-justify">
+                        The GIMJAM 2024 1st place winner, Democrussy is a strategy-card game mixed with some visual novel element to better narrate its epic storyline.
+                        <br />
+                        <br />
+                        In a galaxy far, far away--namely, Susu Kotak Galaxy... After a massive land dispute between the citizens of planet Idgafnesia, Warlok and Magna-Aim, a prophecy has been foretold by a Dukun that ended this major conflict.
+                        "A worthy leader shall be born in the highest peak of Idgafnesia, and they shall bring all conflict to an end" said the Dukun before he vanished.
+                        You ARE the prophecy. Show the citizens of Idgafnesia that you are worthy!
+                        Embark on an epic journey to become the next generation leader of Idgafnesia and continue to maintain peace and harmony on this planet!
+                        Developed in Unity
+                        <br />
+                        <br />
+                        The game was developed only in a single week by sucipto team. Our good team discipline and teamwork is what
+                        has made this possible.
+
                     </p>
                 </div>
             </div>
